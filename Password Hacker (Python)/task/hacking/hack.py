3586089f7baea1a413d9b1361f998fd2e096d9ef
TheodorDolk <theodordolk@gmail.com>
2023-09-10 20:53:55 +0200

    step #2 tests all combinations of passwords and sends to server until access is granted

--- Password Hacker (Python)/task/hacking/hack.py
+++ Password Hacker (Python)/task/hacking/hack.py
@@ -1,20 +1,35 @@
-# step 1
-
+# step 2
+import itertools
 import sys
 import socket
+
 cmd_args = sys.argv
 IP = cmd_args[1]
 port = int(cmd_args[2])
-msg = cmd_args[3]
-msg_byte = msg.encode()
-
 address = (IP, port)
+
+
+def iterate_passwords(client):
+    counter = 1
+    pass_cracked = False
+    while not pass_cracked:
+        repeat = counter
+        passwords = itertools.product("abcdefghijklmnopqrstuvwxyz0123456789", repeat=repeat)
+        # test every combination and send it to server
+        for password_tuple in passwords:
+            password = "".join(password_tuple)
+            password_byte = password.encode()
+            client.send(password_byte)
+            response_byte = client_socket.recv(1024)
+            response = response_byte.decode()
+            if response == "Connection success!":
+                pass_cracked = True
+                print(password)
+            elif response == "Too many attempts":
+                return
+        counter += 1
+
+
 with socket.socket() as client_socket:
     client_socket.connect(address)
-    client_socket.send(msg_byte)
-    response_byte = client_socket.recv(1024)
-response = response_byte.decode()
-print(response)
-
-
-
+    iterate_passwords(client_socket)
