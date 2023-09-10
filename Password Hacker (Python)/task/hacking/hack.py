208d8c671c9bf94d11b61ccbdc4ff144657aeaf2
TheodorDolk <theodordolk@gmail.com>
2023-09-10 20:08:40 +0200

    step #1

---
+++ Password Hacker (Python)/task/hacking/hack.py
@@ -0,0 +1,18 @@
+import sys
+import socket
+cmd_args = sys.argv
+IP = cmd_args[1]
+port = int(cmd_args[2])
+msg = cmd_args[3]
+msg_byte = msg.encode()
+
+address = (IP, port)
+with socket.socket() as client_socket:
+    client_socket.connect(address)
+    client_socket.send(msg_byte)
+    response_byte = client_socket.recv(1024)
+response = response_byte.decode()
+print(response)
+
+
+
